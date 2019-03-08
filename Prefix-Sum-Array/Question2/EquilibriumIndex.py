9202f1dc35415e65c97fb27b19d4c2241151f2b7
Yugander Krishan Singh <ykrishansrangra@gmail.com>
2019-03-09 00:18:58 +0530

    Create EquilibriumIndex.py

---
+++ Prefix-Sum-Array/Question2/EquilibriumIndex.py
@@ -0,0 +1,21 @@
+def getPrefixSumArray(arr):
+    for i in range (1,len(arr)):
+        arr[i] += arr[i-1]
+    
+    
+    
+    
+def getEquilibriumIndex(arr):
+    getPrefixSumArray(arr)
+    equilibriumIndex=-1
+    for i in range(1,len(arr)-1):
+        sum1= arr[i-1]
+        sum2 = arr[len(arr)-1] - arr[i]
+        if sum1 == sum2:
+            equilibriumIndex=i
+            break;
+    return equilibriumIndex
+    
+    
+
+print(getEquilibriumIndex([12,3,4,5]))
